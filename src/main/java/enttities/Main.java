cb23c9e26325a9477a307b9becc3ee51ae5ad34d
Biagio Cota <biagiocota24@gmail.com>
2026-06-12 15:42:59 +0200

    controllo per utente creato

--- src/main/java/enttities/Main.java
+++ src/main/java/enttities/Main.java
@@ -12,6 +12,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
         // 5 VIDEOGIOCHI
         Videogioco videoGame1 = new Videogioco("GTA 5", 70, Piattaforme.PS5, 60, Generi.azione);
         Videogioco videoGame2 = new Videogioco("The Last of Us", 65, Piattaforme.PS5, 50, Generi.azione);
@@ -55,10 +57,121 @@ public class Main {
 //
 //        listaGiochi.getListaGiochi().stream().forEach(gioco -> System.out.println(gioco));
 
-        listaGiochi.stampaStatistiche();
+//        listaGiochi.stampaStatistiche();
 
 //        Gioco giocoPiuCostoso = listaGiochi.getListaGiochi().stream().max(Comparator.comparingDouble(gioco -> gioco.getPrice())).orElse(null);
 //        System.out.println(giocoPiuCostoso);
+
+
+        System.out.println("---------------TUTTI I GIOCHI-----------------");
+        listaGiochi.getListaGiochi().forEach(gioco -> System.out.println(gioco));
+
+        System.out.println("--------------- AZIONI ---------------- ");
+
+        boolean azioneValida = false;
+        int azione = -1;
+        while (azione != 0) {
+
+            do {
+                System.out.println("Quale Azione vuoi eseguire ?");
+                System.out.println("(1)AGGIUNGI | (2)RICERCA CON ID | (3)FILTRA PER PREZZO | (4)FILTRA PER NUMERO | (5)RIMUOVERE ELEMENTO CON ID | (6) AGGIORNAENTO ELEMENTO TRAMITE ID | (7)STAMPA DELLA STATISTICHE DELLA COLLEZIONE");
+                System.out.println("Oppure inserisci 0 per uscire");
+                try {
+                    azione = Integer.parseInt(scanner.nextLine());
+                    if (azione >= 0 && azione <= 7) {
+                        azioneValida = true;
+                    } else {
+                        System.out.println("Inserisci un azione valida !");
+                    }
+                } catch (NumberFormatException e) {
+                    System.err.println("Azione non valida !");
+                }
+            } while (!azioneValida);
+
+
+            switch (azione) {
+                case 0 -> {
+                    break;
+                }
+                case 1 -> {
+                    int tipo = 0;
+                    boolean tipoValido = false;
+                    do {
+                        try {
+                            System.out.println("Vuoi aggiungere (1)Videogioco o (2)Gioco da tavolo ?");
+                            tipo = Integer.parseInt(scanner.nextLine());
+                            if (tipo == 1 || tipo == 2) tipoValido = true;
+                        } catch (NumberFormatException e) {
+                            System.err.println("Tipo gioco non disponibile");
+                        }
+                    } while (!tipoValido);
+                    System.out.println("Insersci il titolo");
+                    String title = scanner.nextLine();
+                    System.out.println("Inserisci il prezzo");
+                    double price = Double.parseDouble(scanner.nextLine());
+                    switch (tipo) {
+                        case 1 -> {
+                            System.out.println("Insersci la piattaforma (PS4 , PS5 , XBOX_ONE , XBOX_SERIE_X , PC)");
+                            Piattaforme piattaforma = Piattaforme.valueOf(scanner.nextLine());
+                            System.out.println("Inserisci la durata del gioco in ore");
+                            int durata = Integer.parseInt(scanner.nextLine());
+                            System.out.println("Insersci il genere (guerra , azione , sport , strategia , combattimento , crime)");
+                            Generi genere = Generi.valueOf(scanner.nextLine());
+                            Videogioco newVideoGame = new Videogioco(title, price, piattaforma, durata, genere);
+                            listaGiochi.addToCollection(newVideoGame);
+                            System.out.println("Gioco aggiunto");
+                        }
+                        case 2 -> {
+                            System.out.println("Inserisci il numero massimo di giocatori da 2  a 10");
+                            int numGiocatori = Integer.parseInt(scanner.nextLine());
+                            System.out.println("Insersci la durata media in minuti ");
+                            int minutiDurata = Integer.parseInt(scanner.nextLine());
+                            GiocoDaTavola newGame = new GiocoDaTavola(title, price, numGiocatori, minutiDurata);
+                            listaGiochi.addToCollection(newGame);
+                            System.out.println("Gioco aggiunto");
+                        }
+                    }
+                }
+                case 2 -> {
+                    System.out.println("Insersci il gameId ");
+                    long gameId = Long.parseLong(scanner.nextLine());
+                    Gioco giocoCercato = listaGiochi.cercaPerId(gameId);
+                    if (giocoCercato != null) System.out.println("Trovato : " + giocoCercato);
+                    else System.out.println("nessun gioco corrispondente all'Id !");
+                }
+                case 3 -> {
+                    System.out.println("Inserisci il prezzo massimo ");
+                    double maxPrice = Double.parseDouble(scanner.nextLine());
+                    List<Gioco> listaFiltrata = listaGiochi.filtraPerPrezzo(maxPrice);
+                    System.out.println("Ecco la lista filtrata :");
+                    listaFiltrata.forEach(gioco -> System.out.println(gioco));
+                }
+                case 4 -> {
+                    System.out.println("Inserisci il numero di giocatori desiderato ");
+                    int numGiocatori = Integer.parseInt(scanner.nextLine());
+                    List<Gioco> listaFiltrata = listaGiochi.cercaPerNumGiocatori(numGiocatori);
+                    System.out.println("Ecco la lista filtrata :");
+                    listaFiltrata.forEach(gioco -> System.out.println(gioco));
+                }
+                case 5 -> {
+                    System.out.println("Inserisci il codice id del gioco che vuoi rimuovere");
+                    long id = Long.parseLong(scanner.nextLine());
+                    Gioco giocoRimosso = listaGiochi.cercaPerId(id); // ✅ prima cerca
+                    if (giocoRimosso != null) {
+                        listaGiochi.removeWithGameId(id);             // poi rimuovi
+                        System.out.println("Gioco " + giocoRimosso.getTitle() + " rimosso");
+                    } else {
+                        System.out.println("Nessun gioco con questo id!");
+                    }
+                }
+                case 6 -> {
+                    System.out.println("Inserisci il codice id del gioco che vuoi modificare ");
+                    long id = Long.parseLong(scanner.nextLine());
+                    listaGiochi.customGame(id);
+                }
+                case 7 -> listaGiochi.stampaStatistiche();
+            }
+        }
 
 
     }
--- src/main/java/enttities/classes/Videogioco.java
+++ src/main/java/enttities/classes/Videogioco.java
@@ -3,6 +3,8 @@ package enttities.classes;
 import enums.Generi;
 import enums.Piattaforme;
 
+import java.util.Scanner;
+
 public class Videogioco extends Gioco {
     private Piattaforme piattaforma;
     private int durataGioco;
@@ -14,6 +16,24 @@ public class Videogioco extends Gioco {
         this.durataGioco = durataGioco;
         this.genere = genere;
     }
+
+//    // COSTRUTTORE PER UTENTE
+//    public Videogioco() {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Insersci il titolo");
+//        String title = scanner.nextLine();
+//        System.out.println("Inserisci il prezzo");
+//        double price = Double.parseDouble(scanner.nextLine());
+//        super(title, price);
+//        System.out.println("Insersci la piattaforma (PS4 , PS5 , XBOX_ONE , XBOX_SERIE_X , PC)");
+//        Piattaforme piattaforma = Piattaforme.valueOf(scanner.nextLine());
+//        this.piattaforma = piattaforma;
+//        System.out.println("Inserisci la durata del gioco in ore");
+//        this.durataGioco = Integer.parseInt(scanner.nextLine());
+//        System.out.println("Insersci il genere (guerra , azione , sport , strategia , combattimento , crime)");
+//        Generi genere = Generi.valueOf(scanner.nextLine());
+//        this.genere = genere;
+//    }
 
     public void setPiattaforma(Piattaforme piattaforma) {
         this.piattaforma = piattaforma;
